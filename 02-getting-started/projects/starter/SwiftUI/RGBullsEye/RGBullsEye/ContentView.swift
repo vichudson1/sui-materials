683f259766976e1b29174deacecd42fbcbd57106
Victor Hudson <vichudson1@gmail.com>
2021-10-03 09:48:13 -0400

    Progress
    Signed-off-by: Victor Hudson <vichudson1@gmail.com>

--- 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
+++ 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
@@ -33,14 +33,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var game = Game()
+    @State var guess: RGB
+    var target = RGB.random()
+
+
+    
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+      VStack {
+          Color(rgbStruct: game.target)
+          Text("R: ??? G: ??? B: ???")
+              .padding()
+          
+          Color(rgbStruct: guess)
+          Text("R: 204 G: 76 B: 178")
+              .padding()
+          
+          HStack {
+              Text("0")
+              Slider(value: $guess.red)
+                  .accentColor(.red)
+              Text("255")
+          }
+          .padding(.horizontal)
+          Button(action: {}) {
+                  Text("Hit Me!")
+              }
+      }
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+    ContentView(guess: RGB(red: 0.3, green: 0.3, blue: 0.7))
   }
 }
